0e27b24e8deb526e70fc21cd077bea509f0c353b
Sean <seantsusmilch@gmail.com>
2021-11-27 18:18:33 -0600

    ⛏️Defining structure of the project

---
+++ helpers.py
@@ -0,0 +1,4 @@
+'''
+This file will have helper functions that assist the processor
+I'm thinking like getting the input data, and printing the output table
+'''
---
+++ processor.py
@@ -0,0 +1,7 @@
+'''
+This file will have all the actual processor stuff
+'''
+
+
+register_file = [0] * 8
+rat = [0] * 5
