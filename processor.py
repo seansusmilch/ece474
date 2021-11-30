e907ae60193b387bed56878bf98816b4cb6cb8e8
Sean <seantsusmilch@gmail.com>
2021-11-30 16:28:53 -0600

    🥺Starting processor

--- processor.py
+++ processor.py
@@ -2,9 +2,18 @@
 This file will have all the actual processor stuff
 '''
 from rezsta import ResStation
+from helpers import parse_input
 
 register_file = [0] * 8
 rat = [0] * 5
 
 add_rs = [ResStation(11),ResStation(12),ResStation(13)]
 mul_rs = [ResStation(14),ResStation(15)]
+
+cycles, instructions, rf = parse_input()
+
+for x in range(cycles):
+    if instructions:
+        # try to issue instruction
+        op_code, dest, rleft, rright = instructions[0]
+        
