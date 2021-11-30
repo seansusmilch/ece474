0c3445646c71d8a08b70d6a5ad437a8479b33917
Sean <seantsusmilch@gmail.com>
2021-11-30 16:11:58 -0600

    🚂Added Reservation Station

--- processor.py
+++ processor.py
@@ -1,7 +1,10 @@
 '''
 This file will have all the actual processor stuff
 '''
-
+from rezsta import ResStation
 
 register_file = [0] * 8
 rat = [0] * 5
+
+add_rs = [ResStation(11),ResStation(12),ResStation(13)]
+mul_rs = [ResStation(14),ResStation(15)]
